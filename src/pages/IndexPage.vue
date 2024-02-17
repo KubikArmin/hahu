0a180615f17f0175a2a5741bea03fc6b98647f8f
KubikArmin <kubikarminmartin@gmail.com>
2024-02-17 18:38:01 +0100

    Work in progress show all advertisements

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import AdvertisementCard from "src/components/AdvertisementCard.vue";
 import CategorySelector from "../components/CategorySelector.vue";
+import {ads} from '../ads'
+
+const advertisements = ads
 </script>
 
 <template>
-  <q-page>
-    <CategorySelector></CategorySelector>
-
-    
+  <q-page class="q-mx-auto">
+    <div class="row justify-center">
+      <CategorySelector/>
+    </div>
+    <div class="row justify-center">
+      <AdvertisementCard class="col-lg-4 col-md-6 col-sm-12" />
+      <AdvertisementCard class="col-lg-4 col-md-6 col-sm-12" />
+      <AdvertisementCard class="col-lg-4 col-md-6 col-sm-12" />
+      <AdvertisementCard class="col-lg-4 col-md-6 col-sm-12" />
+    </div>
   </q-page>
 </template>
 
