8170797efcaf41f148e56b52ca8edce061e5ab93
KubikArmin <kubikarminmartin@gmail.com>
2024-02-17 18:33:47 +0100

    Fixed bug for max-width

--- src/components/AdvertisementCard.vue
+++ src/components/AdvertisementCard.vue
@@ -68,6 +68,7 @@ let desc =
 let expanded = ref(false);
 let lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
 </script>
 
 <template>
@@ -137,5 +138,4 @@ let lorem =
 <style lang="sass" scoped>
 .my-card
   width: 100%
-  max-width: 350px
 </style>
