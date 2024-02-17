ac0a7dd10546fc122cc7bd1e20b2439b50f4d506
Gere Kovács Dávid <geresmithdavid@gmail.com>
2024-02-17 18:30:47 +0100

    store.ts, IndexPage update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import CategorySelector from "../components/CategorySelector.vue";
+//import EditDialogComponent from "../components/EditDialogComponent.vue";
+import { useStore } from "../stores/store";
+const store = useStore();
+
+//store.app.showEditDialog = true;
 </script>
 
 <template>
   <q-page>
     <CategorySelector></CategorySelector>
-
-    
+    <EditDialogComponent></EditDialogComponent>
   </q-page>
 </template>
 
